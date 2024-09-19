00634419aa67fefbf765414f9015d969818cdb57
golisrikanth1989 <golisrikanth1989@gmail.com>
2024-09-19 11:48:02 +0530

    donr

--- Project_Sample/internal/routes/routes.go
+++ Project_Sample/internal/routes/routes.go
@@ -1,24 +1,24 @@
 package routes
-import {
+
+import (
 	"fmt"
 	"net/http"
-}
+)
 
-func NewRouter() http.Handler{
-	mux:= http.NewServerMux()
+func NewRouter() http.Handler {
+	mux := http.NewServerMux()
 
-	mux.HandleFunc("/",indexHandler)
-	mux.HandleFunc("/api/data",apiDataHandler)
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/api/data", apiDataHandler)
 
 	return mux
 }
 
-
-func indexHandler(w http.ResponseWriter,r  *http.Request){
-	fmt.Fprintln(w,"Welcome to Home Page!")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to Home Page!")
 }
 
-func apiDataHandler(w http.ResponseWriter,r *http.Request){
+func apiDataHandler(w http.ResponseWriter, r *http.Request) {
 	data := "Some Data from API"
-	fmt.Fprintln(w,data)
+	fmt.Fprintln(w, data)
 }
